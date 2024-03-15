de13561b8f1726110c661f769406817e2166902b
Alex Argueta <alexarguetanet@gmail.com>
2024-03-14 23:11:27 -0400

    Implemented controller functions for user authentication

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { authRouter } from "./routes/auth";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,10 +11,13 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use("/auth", authRouter);
 
 mongoose.connect(process.env.MONGO_URI || "")
 .then(() => {
-    console.log(`Connected to mongodb! Server running on PORT ${PORT}`);
+    app.listen(PORT, () => {
+        console.log(`Connected to mongodb! Server running on PORT ${PORT}`);
+    });
 })
 .catch(err => console.log(err));
 
